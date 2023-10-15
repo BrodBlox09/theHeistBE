cab3402c50037e7f92520892da3cb52de5e2fee6
BrodBlox09 <brodenskicomp@outlook.com>
2023-10-15 13:46:15 +0200

    Update DataManager.ts

--- theHeist_BP/src/DataManager.ts
+++ theHeist_BP/src/DataManager.ts
@@ -2,22 +2,22 @@ import { Entity, world } from '@minecraft/server';
 
 export default class DataManager {
 	static getData(entity: Entity, dataNodeName: string) {
-		let dataStr = entity.getDynamicProperty('data') as string;
+		const dataStr = entity.getDynamicProperty('data') as string;
 		if (dataStr == null) return;
-		let dataNodes = JSON.parse(dataStr);
-		let dataNode = dataNodes.find((x: any) => (x.name == dataNodeName));
+		const dataNodes = JSON.parse(dataStr);
+		const dataNode = dataNodes.find((x: any) => (x.name == dataNodeName));
 		if (!dataNode) return undefined;
 		else return dataNode;
 	}
 
 	static setData(entity: Entity, dataNodeName: string, object: object) {
-		let dataStr = entity.getDynamicProperty('data') as string;
+		const dataStr = entity.getDynamicProperty('data') as string;
 		if (!dataStr) {
 			entity.setDynamicProperty('data', JSON.stringify([object]));
 			return true;
 		}
-		let dataNodes = JSON.parse(dataStr);
-		let dataNodeIndex = dataNodes.findIndex((x: any) => (x.name == dataNodeName));
+		const dataNodes = JSON.parse(dataStr);
+		const dataNodeIndex = dataNodes.findIndex((x: any) => (x.name == dataNodeName));
 		if (dataNodeIndex == -1) {
 			dataNodes.push(object);
 			entity.setDynamicProperty('data', JSON.stringify(dataNodes));
