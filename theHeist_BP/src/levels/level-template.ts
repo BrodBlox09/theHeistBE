25981b021985d3bcf0a84d97c31478e2239dd248
BrodBlox09 <brodenskicomp@outlook.com>
2025-11-12 21:33:51 +0100

    Update level-template.ts

--- theHeist_BP/src/levels/level-template.ts
+++ theHeist_BP/src/levels/level-template.ts
@@ -1,4 +1,5 @@
 import Vector from "../Vector";
+import VectorXZ from "../VectorXZ";
 import Utilities from "../Utilities";
 import LevelConstructor from "./LevelConstructor";
 import { ILevel, BlockRotation } from "../TypeDefinitions";
@@ -21,8 +22,8 @@ const level: ILevel = {
 setup: () => {
 
 {
-    LevelConstructor.door4(new Vector(0, 0, 0), new Vector(0, 0, 0), BlockRotation.NORTH);
-    LevelConstructor.keycardReader(new Vector(0, 0, 0), "COLOR", [
+    LevelConstructor.door4(new VectorXZ(0, 0), new VectorXZ(0, 0), BlockRotation.NORTH);
+    LevelConstructor.keycardReader(new VectorXZ(0, 0), "COLOR", [
         {
             "type": "set_block", "do": { "x": 0, "y": 0, "z": 0, "block": "theheist:custom_door_4_bottom_l", "permutations": { "minecraft:cardinal_direction": "north", "theheist:open": true } }, "delay": 40
         },
@@ -36,8 +37,8 @@ setup: () => {
             "type": "manage_objective", "do": { "manageType": 2, "objective": "Access next level" }, "delay": 40
         }
     ]);
-    Utilities.setBlock(new Vector(0, 0, 0), "minecraft:lever", { "lever_direction": "north" });
-    LevelConstructor.rechargeStation(new Vector(0, 0, 0), BlockRotation.NORTH);
+    Utilities.setBlock(new Vector(0, Utilities.consoleDisplayHeight, 0), "minecraft:lever", { "lever_direction": "north" });
+    LevelConstructor.rechargeStation(new VectorXZ(0, 0), BlockRotation.NORTH);
 } // End Level Elevator
 
 }
